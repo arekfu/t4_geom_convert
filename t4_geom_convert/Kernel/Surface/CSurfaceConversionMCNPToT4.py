8413095afcf7634bc7f39bd0d3a4cd1fb35e9ae1
Davide Mancusi <davide.mancusi@cea.fr>
2019-05-09 09:19:01 +0200

    Properly handle surface conversion errors

--- t4_geom_convert/Kernel/Surface/CSurfaceConversionMCNPToT4.py
+++ t4_geom_convert/Kernel/Surface/CSurfaceConversionMCNPToT4.py
@@ -55,43 +55,62 @@ class CSurfaceConversionMCNPToT4(object):
         if p_typeSurfaceMCNP.name == "PX" or p_typeSurfaceMCNP.name == "PY" or\
         p_typeSurfaceMCNP.name == "PZ" and len(p_listeParametreMCNP) == 1:
             listeParametreT4 = p_listeParametreMCNP
-        if p_typeSurfaceMCNP.name == "P" or p_typeSurfaceMCNP.name == "S" and\
+        elif p_typeSurfaceMCNP.name == "P" or p_typeSurfaceMCNP.name == "S" and\
         len(p_listeParametreMCNP) == 4:
             listeParametreT4 = p_listeParametreMCNP
-        if p_typeSurfaceMCNP.name == "C/X" or p_typeSurfaceMCNP.name == "C/Y" or\
+        elif p_typeSurfaceMCNP.name == "C/X" or p_typeSurfaceMCNP.name == "C/Y" or\
         p_typeSurfaceMCNP.name == "C/Z"  and len(p_listeParametreMCNP) == 3:
             listeParametreT4 = p_listeParametreMCNP
-        if p_typeSurfaceMCNP.name == "GQ"  and len(p_listeParametreMCNP) == 8:
+        elif p_typeSurfaceMCNP.name == "GQ"  and len(p_listeParametreMCNP) == 8:
             listeParametreT4 = p_listeParametreMCNP
-        if p_typeSurfaceMCNP.name == "SO"  and len(p_listeParametreMCNP) == 1:
+        elif p_typeSurfaceMCNP.name == "SO"  and len(p_listeParametreMCNP) == 1:
             listeParametreT4 = [0, 0, 0, p_listeParametreMCNP[0]]
-        if p_typeSurfaceMCNP.name == "SX"  and len(p_listeParametreMCNP) == 2:
+        elif p_typeSurfaceMCNP.name == "SX"  and len(p_listeParametreMCNP) == 2:
             listeParametreT4 = [p_listeParametreMCNP[0], 0, 0, p_listeParametreMCNP[1]]
-        if p_typeSurfaceMCNP.name == "SY"  and len(p_listeParametreMCNP) == 2:
+        elif p_typeSurfaceMCNP.name == "SY"  and len(p_listeParametreMCNP) == 2:
             listeParametreT4 = [0, p_listeParametreMCNP[0], 0, p_listeParametreMCNP[1]]
-        if p_typeSurfaceMCNP.name == "SZ"  and len(p_listeParametreMCNP) == 2:
+        elif p_typeSurfaceMCNP.name == "SZ"  and len(p_listeParametreMCNP) == 2:
             listeParametreT4 = [0, 0, p_listeParametreMCNP[0], p_listeParametreMCNP[1]]
-        if p_typeSurfaceMCNP.name == "CX" or p_typeSurfaceMCNP.name == "CY" or\
+        elif p_typeSurfaceMCNP.name == "CX" or p_typeSurfaceMCNP.name == "CY" or\
         p_typeSurfaceMCNP.name == "CZ"  and len(p_listeParametreMCNP) == 1:
             listeParametreT4 = [0, 0, p_listeParametreMCNP[0]]
-        if p_typeSurfaceMCNP.name == "K/X"  or p_typeSurfaceMCNP.name == "K/Y"\
-        or p_typeSurfaceMCNP.name == "K/Z":
+        elif (p_typeSurfaceMCNP.name == "K/X"
+              or p_typeSurfaceMCNP.name == "K/Y"
+              or p_typeSurfaceMCNP.name == "K/Z"):
             p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[3])))
             if len(p_listeParametreMCNP) == 4:
-                listeParametreT4 = [p_listeParametreMCNP[0], p_listeParametreMCNP[1],\
+                listeParametreT4 = [p_listeParametreMCNP[0],
+                                    p_listeParametreMCNP[1],
                                     p_listeParametreMCNP[2], p_atant]
-            if len(p_listeParametreMCNP) == 5:
-                listeParametreT4 = [p_listeParametreMCNP[0], p_listeParametreMCNP[1],\
-                                    p_listeParametreMCNP[2], p_atant,\
+            elif len(p_listeParametreMCNP) == 5:
+                listeParametreT4 = [p_listeParametreMCNP[0],
+                                    p_listeParametreMCNP[1],
+                                    p_listeParametreMCNP[2], p_atant,
                                     p_listeParametreMCNP[4]]
-        if p_typeSurfaceMCNP.name == "KX"  and len(p_listeParametreMCNP) == 2:
-            p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[1])))
-            listeParametreT4 = [p_listeParametreMCNP[0], 0, 0, p_atant]
-        if p_typeSurfaceMCNP.name == "KY"  and len(p_listeParametreMCNP) == 2:
-            p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[1])))
-            listeParametreT4 = [0, p_listeParametreMCNP[0], 0, p_atant]
-        if p_typeSurfaceMCNP.name == "KZ"  and len(p_listeParametreMCNP) == 2:
-            p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[1])))
-            listeParametreT4 = [0, 0, p_listeParametreMCNP[0], p_atant]
+            else:
+                msg = ('Unexpected number of MCNP parameters in {} surface: {}'
+                       .format(p_typeSurfaceMCNP, len(p_listeParametreMCNP)))
+                raise ValueError(msg)
+        elif p_typeSurfaceMCNP.name == "KX":
+            if len(p_listeParametreMCNP) == 2:
+                p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[1])))
+                listeParametreT4 = [p_listeParametreMCNP[0], 0, 0, p_atant]
+            else:
+                raise ValueError('Cannot handle 1-nappe KX cones (yet)')
+        elif p_typeSurfaceMCNP.name == "KY":
+            if len(p_listeParametreMCNP) == 2:
+                p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[1])))
+                listeParametreT4 = [0, p_listeParametreMCNP[0], 0, p_atant]
+            else:
+                raise ValueError('Cannot handle 1-nappe KY cones (yet)')
+        elif p_typeSurfaceMCNP.name == "KZ":
+            if len(p_listeParametreMCNP) == 2:
+                p_atant = math.atan(math.sqrt(float(p_listeParametreMCNP[1])))
+                listeParametreT4 = [0, 0, p_listeParametreMCNP[0], p_atant]
+            else:
+                raise ValueError('Cannot handle 1-nappe KZ cones (yet)')
+        else:
+            raise ValueError('Cannot convert MCNP surface: {} {}'
+                             .format(p_typeSurfaceMCNP, p_listeParametreMCNP))
 
         return listeParametreT4
--- t4_geom_convert/Kernel/Surface/ESurfaceTypeMCNP.py
+++ t4_geom_convert/Kernel/Surface/ESurfaceTypeMCNP.py
@@ -6,8 +6,8 @@ Created on 19 févr. 2019
 '''
 from enum import Enum
 
-ESurfaceTypeMCNP = Enum('ESurfaceTypeMCNP', 'PX PY PZ P SO S SX SY SZ C/X \
-C/Y C/Z CX CY CZ K/X K/Y K/Z KX KY KZ GQ TX TY TZ')
+ESurfaceTypeMCNP = Enum('ESurfaceTypeMCNP', 'PX PY PZ P SO S SX SY SZ C/X '
+                        'C/Y C/Z CX CY CZ K/X K/Y K/Z KX KY KZ GQ TX TY TZ')
 
 # yo = ESurfaceTypeMCNP
 # print(yo.PX.name == 'PX')
