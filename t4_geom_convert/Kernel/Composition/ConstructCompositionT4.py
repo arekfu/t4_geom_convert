4ec1a0bf5ddd4037796b90aee757d7afcb83f00a
Davide Mancusi <davide.mancusi@cea.fr>
2019-10-01 19:19:02 +0200

    Correctly handle the conversion of POINT_WISE compositions
    Fixes issue #14.
    
    This commit also adds a warning when it detects an MCNP composition that
    was specified with mass fractions (negative values in the Mn card), but
    used with a total atom concentration in a cell (in atoms/cm³).  At the
    moment we do not support this because it would require the use of an
    atomic mass table (probably MCNP's?).

--- t4_geom_convert/Kernel/Composition/ConstructCompositionT4.py
+++ t4_geom_convert/Kernel/Composition/ConstructCompositionT4.py
@@ -6,6 +6,7 @@ Created on 6 févr. 2019
 :data : 06 february 2019
 '''
 from collections import OrderedDict
+from math import fsum
 
 from .CCompositionT4 import CCompositionT4
 from .CompositionConversionMCNPToT4 import compositionConversionMCNPToT4
@@ -16,31 +17,73 @@ def constructCompositionT4(mcnp_parser, dic_cell_mcnp):
     :brief: method changing the tuple from compositionConversionMCNPToT4
     in instance of the CVolumeT4 Class
     '''
-    dic_new_composition_t4 = OrderedDict()
+    dic_new_composition = OrderedDict()
     for key, val in compositionConversionMCNPToT4(mcnp_parser).items():
-        composition = []
-        densities = []
-        type_density_t4 = []
-        for (enum_element, mass_number), abundance in val.isotopes:
-            if mass_number.startswith('0'):
-                # remove leading zeros
-                mass_number = str(int(mass_number))
-            isotope_t4 = enum_element.name + mass_number
-            composition.append((isotope_t4, abundance))
-        for cell in dic_cell_mcnp.values():
+        fractions = extract_isotopes_fractions(val.isotopes)
+        densities = set()
+        for cell_id, cell in dic_cell_mcnp.items():
             if (cell.importance <= 0. or cell.universe != 0
                     or cell.fillid is not None):
                 continue
-            if int(cell.materialID) == key:
-                density = cell.density
-                if density not in densities:
-                    densities.append(density)
-                    if float(density) < 0:
-                        type_density_t4.append('DENSITY')
-                    else:
-                        type_density_t4.append('POINT_WISE')
-        dic_new_composition_t4[key] = CCompositionT4(type_density_t4,
-                                                     'm'+str(key), densities,
-                                                     composition,
-                                                     val.atom_fracs)
-    return dic_new_composition_t4
+            if int(cell.materialID) != key:
+                continue
+            density = cell.density
+            if density in densities:
+                continue
+            densities.add(density)
+            if float(density) < 0:
+                type_density_t4 = 'DENSITY'
+                compo = fractions.copy()
+            else:
+                # check that the composition is specified by atomic
+                # fractions; mass fractions + positive concentration in
+                # the cell are not supported for the moment
+                type_density_t4 = 'POINT_WISE'
+                if not val.atom_fracs:
+                    print('\nWARNING: composition {} cannot be '
+                          'correctly converted in cell {} because '
+                          'total concentrations ({}) with mass '
+                          'fractions are not supported at the moment'
+                          .format(key, cell_id, density))
+                    compo = []
+                else:
+                    compo = rescale_fractions(fractions, float(density))
+
+            if key not in dic_new_composition:
+                dic_new_composition[key] = []
+            new_compo = CCompositionT4(type_density_t4, 'm'+str(key),
+                                       density, compo, val.atom_fracs)
+            dic_new_composition[key].append(new_compo)
+    return dic_new_composition
+
+
+def extract_isotopes_fractions(isotopes):
+    '''Extract the list of isotopes and the respective fractions from the MCNP
+    parsed composition.'''
+    fractions = []
+    for (enum_element, mass_number), abundance in isotopes:
+        if mass_number.startswith('0'):
+            # remove leading zeros
+            mass_number = str(int(mass_number))
+        isotope_t4 = enum_element.name + mass_number
+        fractions.append((isotope_t4, abundance))
+    return fractions
+
+
+def rescale_fractions(fractions, concentration):
+    '''Rescale the given atomic fractions so that the total concentration
+    equals the given value.
+
+    :param fractions: list of ``(isotope, fraction)`` pairs, as strings
+    :type fractions: list((str, str))
+    :param float concentration: the total concentration
+    :returns: a list of ``(isotope, concentration)`` pairs, as strings
+    :rtype: list((str, str))
+    '''
+    conc_fmt = '{:.15e}'
+    concs = []
+    total_fractions = fsum(float(frac) for _, frac in fractions)
+    for isotope, frac in fractions:
+        conc_str = conc_fmt.format(float(frac)*concentration/total_fractions)
+        concs.append((isotope, conc_str))
+    return concs
--- t4_geom_convert/Kernel/FileHandlers/Writer/WriteT4Composition.py
+++ t4_geom_convert/Kernel/FileHandlers/Writer/WriteT4Composition.py
@@ -18,22 +18,22 @@ def writeT4Composition(mcnpParser, mcnp_new_dict, ofile):
     temperature = 300
     dic_composition = constructCompositionT4(mcnpParser, mcnp_new_dict)
     n_compos = 0
-    for mat in dic_composition.values():
-        n_compos += len(mat.valueOfDensity)
+    for mats in dic_composition.values():
+        n_compos += len(mats)
     ofile.write(str(n_compos + 1) + "\n")  # +1 from the m0 (void) composition
 
     n_mcnp_compos = len(dic_composition)
     fmt_string = ('\rconverting composition {{:{}d}}/{}'
                   .format(len(str(n_mcnp_compos)), n_mcnp_compos))
-    for j, mat in enumerate(dic_composition.values()):
+    for j, mats in enumerate(dic_composition.values()):
         print(fmt_string.format(j+1), end='', flush=True)
-        l_typeDensity = mat.typeDensity
-        l_densityValue = mat.valueOfDensity
-        list_isotope = mat.listMaterialComposition
-        p_numberOfIsotope = len(list_isotope)
-        isotopes_str = '\n  '.join(name + ' ' + abundance
-                                   for name, abundance in list_isotope)
-        for typ, density in zip(l_typeDensity, l_densityValue):
+        for mat in mats:
+            typ = mat.typeDensity
+            density = mat.valueOfDensity
+            list_isotope = mat.listMaterialComposition
+            p_numberOfIsotope = len(list_isotope)
+            isotopes_str = '\n  '.join(name + ' ' + abundance
+                                       for name, abundance in list_isotope)
             p_materialName = mat.material + '_' + density
             if typ == 'POINT_WISE':
                 ofile.write("{} {:d} {} {:d}\n  {}\n"
@@ -45,9 +45,5 @@ def writeT4Composition(mcnpParser, mcnp_new_dict, ofile):
                                     p_materialName, str_fabs(density),
                                     'NB_ATOM' if mat.nb_atom else '',
                                     p_numberOfIsotope, isotopes_str))
-    ofile.write("POINT_WISE 300 m0 1 HE4 1E-30\n")
+    ofile.write("POINT_WISE 300 m0 1\n  HE4 1E-30\n\nEND_COMPOSITION\n")
     print('... done', flush=True)
-
-    ofile.write("\n")
-    ofile.write("END_COMPOSITION")
-    ofile.write("\n")
