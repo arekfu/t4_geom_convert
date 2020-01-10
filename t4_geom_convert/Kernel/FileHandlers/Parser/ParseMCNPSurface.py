da3af4d35ea451cd296a8d3d8cf2f3910e03922c
Davide Mancusi <davide.mancusi@cea.fr>
2020-01-10 10:54:07 +0100

    Refactor ParseMCNPSurface in preparation for the implementation of macro-bodies

--- t4_geom_convert/Kernel/FileHandlers/Parser/ParseMCNPSurface.py
+++ t4_geom_convert/Kernel/FileHandlers/Parser/ParseMCNPSurface.py
@@ -17,41 +17,25 @@ from ...Transformation.Transformation import transformation
 from ...VectUtils import planeParamsFromPoints
 
 
-def parseMCNPSurface(mcnpParser):
+def parseMCNPSurface(mcnp_parser):
     '''
     :brief method which permit to recover the information of each line
     of the block SURFACE
     :return: dictionary which contains the ID of the surfaces as a key
     and as a value, a object from the class CSurfaceMCNP
     '''
-    surfaceParsed = get_surfaces(mcnpParser, lim=None)
-    transformParsed = get_transforms(mcnpParser, lim=None)
-    dictSurface = OrderedDict()
-    n_surf = len(surfaceParsed)
+    surface_parsed = get_surfaces(mcnp_parser, lim=None)
+    transform_parsed = get_transforms(mcnp_parser, lim=None)
+    dict_surface = OrderedDict()
+    n_surf = len(surface_parsed)
     fmt_string = '\rparsing MCNP surface {{:{}d}}/{}'.format(len(str(n_surf)),
                                                              n_surf)
-    for i, (k, v) in enumerate(surfaceParsed.items()):
+    for i, (key, surface) in enumerate(surface_parsed.items()):
         print(fmt_string.format(i+1), end='', flush=True)
-        p_boundCond, p_transformation, p_typeSurface, l_paramSurface = v
-        enumSurface = string_to_enum(p_typeSurface)
-        enumSurface, l_paramSurface = normalizeSurface(enumSurface,
-                                                       l_paramSurface)
-        t, l_paramSurface, l_complParam, _ = mcnp2cad[mcnp_to_mip(enumSurface)](l_paramSurface)
-        enumSurface = string_to_enum(t)
-        idorigin = [k]
-        if p_transformation:
-            idorigin.append('via transformation {}'
-                            .format(int(p_transformation)))
-            surf = transformation(transformParsed[int(p_transformation)],
-                                  enumSurface, l_paramSurface, l_complParam,
-                                  p_boundCond, idorigin)
-        else:
-            surf = CSurfaceMCNP(p_boundCond, enumSurface, l_paramSurface,
-                                l_complParam, idorigin)
-        dictSurface[k] = surf
+        dict_surface[key] = to_surface_mcnp(key, surface, transform_parsed)
     print('... done', flush=True)
 
-    return dictSurface
+    return dict_surface
 
 
 def normalizeSurface(typ, params):
@@ -61,9 +45,31 @@ def normalizeSurface(typ, params):
     if typ == ESurfaceTypeMCNP.P:
         if len(params) == 9:
             params = planeParamsFromPoints(params[0:3],
-                                        params[3:6],
-                                        params[6:9])
+                                           params[3:6],
+                                           params[6:9])
         elif len(params) != 4:
             raise ValueError('Planes "P" expect either 4 or 9 parameters')
 
     return typ, params
+
+
+def to_surface_mcnp(key, parsed_surface, transform_parsed):
+    '''Convert the surface described by the given type parameters to a
+    :class:`CSurfaceMCNP`.'''
+    bound_cond, transform_id, type_surface, params = parsed_surface
+    enum_surface = string_to_enum(type_surface)
+    enum_surface, params = normalizeSurface(enum_surface, params)
+    mip_transf = mcnp2cad[mcnp_to_mip(enum_surface)]
+    typ, params, compl_params, _ = mip_transf(params)
+    enum_surface = string_to_enum(typ)
+    idorigin = [key]
+    if transform_id:
+        idorigin.append('via transformation {}'
+                        .format(int(transform_id)))
+        surf = transformation(transform_parsed[int(transform_id)],
+                              enum_surface, params, compl_params,
+                              bound_cond, idorigin)
+    else:
+        surf = CSurfaceMCNP(bound_cond, enum_surface, params,
+                            compl_params, idorigin)
+    return surf
