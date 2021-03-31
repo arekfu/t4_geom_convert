5ada1246627e55be0afdea0c11e708e9bb1dd0a5
Davide Mancusi <davide.mancusi@cea.fr>
2021-03-31 10:39:03 +0200

    Switch to poetry for the packaging

--- t4_geom_convert/__init__.py
+++ t4_geom_convert/__init__.py
@@ -17,4 +17,9 @@
 #
 # vim: set fileencoding=utf-8 :
 
-__version__ = '0.4.1'
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
