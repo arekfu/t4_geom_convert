arekfu/t4_geom_convert

commit ef8c146608a0befafc529bb68c1e41874571c50d

Handle Fortran-style scientific notation in densities and concentrations

t4_geom_convert/Kernel/FileHandlers/Parser/ParseMCNPCell.py
@@ -188,7 +188,7 @@ class ParseMCNPCell:
         if int(material_id) == 0:
             density = None
         else:
-            density = normalize_density(material.split()[1])
+            density = material.split()[1]
         return material_id, density
 
     @staticmethod
@@ -245,7 +245,7 @@ class ParseMCNPCell:
                 keywords['u'] = int(float(kw_list.pop()))
             elif 'rho' in elt:
                 # only relevant for LIKE n BUT cells
-                keywords['density'] = normalize_density(kw_list.pop())
+                keywords['density'] = kw_list.pop()
             elif 'mat' in elt:
                 # only relevant for LIKE n BUT cells
                 keywords['material'] = kw_list.pop()
@@ -328,28 +328,3 @@ class ParseMCNPCell:
         elif '*' in elt:
             trcl_params[3:] = list(map(to_cos, trcl_params[3:12]))
         return tuple(trcl_params)
-
-
-def normalize_density(density):
-    '''Return a normalized version of the density, removing any trailing
-    zeros, except one if it is the only digit after the decimal point.
-
-    Examples:
-
-    >>> normalize_density('1.0')
-    '1.0'
-    >>> normalize_density('1.23000')
-    '1.23'
-    >>> normalize_density('-1.23000')
-    '-1.23'
-    >>> normalize_density('-.23000')
-    '-.23'
-    >>> normalize_density('7')
-    '7'
-    >>> normalize_density('10')
-    '10'
-    '''
-    norm = re.sub(r'^([-+]?[0-9]*\.[0-9]*[^0])0+$', r'\1', density)
-    if norm[-1] == '.':
-        norm += '0'
-    return norm

t4_geom_convert/Kernel/Composition/ConstructCompositionT4.py
@@ -20,6 +20,7 @@
 from collections import OrderedDict
 from math import fsum
 
+from ..Utils import normalize_float
 from .CCompositionT4 import CCompositionT4
 from .CompositionConversionMCNPToT4 import compositionConversionMCNPToT4
 
@@ -41,7 +42,8 @@ def constructCompositionT4(mcnp_parser, dic_cell_mcnp):
             if density in densities:
                 continue
             densities.add(density)
-            if float(density) < 0:
+            fdensity = float(normalize_float(density))
+            if fdensity < 0.0:
                 type_density_t4 = 'DENSITY'
                 compo = fractions.copy()
             else:
@@ -57,12 +59,13 @@ def constructCompositionT4(mcnp_parser, dic_cell_mcnp):
                           .format(key, cell_id, density))
                     compo = []
                 else:
-                    compo = rescale_fractions(fractions, float(density))
+                    compo = rescale_fractions(fractions, fdensity)
 
             if key not in dic_new_composition:
                 dic_new_composition[key] = []
             new_compo = CCompositionT4(type_density_t4, 'm' + str(key),
-                                       density, compo, val.atom_fracs)
+                                       normalize_float(density),
+                                       compo, val.atom_fracs)
             dic_new_composition[key].append(new_compo)
     return dic_new_composition
 
@@ -92,9 +95,10 @@ def rescale_fractions(fractions, concentration):
     '''
     conc_fmt = '{:.15e}'
     concs = []
-    total_fractions = fsum(float(frac) for _, frac in fractions)
+    total_fractions = fsum(float(normalize_float(frac))
+                           for _, frac in fractions)
     for isotope, frac in fractions:
         conc_str = conc_fmt.format(
-            float(frac) * concentration / total_fractions)
+            float(normalize_float(frac)) * concentration / total_fractions)
         concs.append((isotope, conc_str))
     return concs

t4_geom_convert/Kernel/Utils.py
@@ -1,0 +1,60 @@
+# Copyright 2019-2021 Davide Mancusi, Martin Maurey, Jonathan Faustin
+#
+# This file is part of t4_geom_convert.
+#
+# t4_geom_convert is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# t4_geom_convert is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# t4_geom_convert.  If not, see <https://www.gnu.org/licenses/>.
+#
+# vim: set fileencoding=utf-8 :
+
+import re
+
+
+def normalize_float(density):
+    '''Return a normalized version of the density, removing any trailing
+    zeros, except one if it is the only digit after the decimal point. Also
+    normalizes the Fortran scientific notation (``1.2-4 == 1.2e-4``).
+
+    Examples:
+
+    >>> normalize_float('1.0')
+    '1.0'
+    >>> normalize_float('1.23000')
+    '1.23'
+    >>> normalize_float('-1.23000')
+    '-1.23'
+    >>> normalize_float('-.23000')
+    '-.23'
+    >>> normalize_float('7')
+    '7'
+    >>> normalize_float('10')
+    '10'
+    >>> normalize_float('6.40875-2')
+    '6.40875e-2'
+    >>> normalize_float('6.40875+2')
+    '6.40875e+2'
+    >>> normalize_float('-6.40875-2')
+    '-6.40875e-2'
+    >>> normalize_float('-.40875-2')
+    '-.40875e-2'
+    >>> normalize_float('1.-2')
+    '1.e-2'
+    >>> normalize_float('6.3023-5')
+    '6.3023e-5'
+    '''
+    norm = re.sub(r'^([-+]?[0-9]*\.[0-9]*[^0])0+$', r'\1', density)
+    if norm[-1] == '.':
+        norm += '0'
+    norm = re.sub(r'^([-+]?[0-9]*\.[0-9]*)[eEdD]?([-+][0-9]+)$', r'\1e\2',
+                  norm)
+    return norm

commit 52a849a399c5656ed63b1005c758198b0e32944f

Fix bugs introduced by commit ef8c146608a0befafc529bb68c1e41874571c50d

Oracle/src/MCNPGeometry.cc
@@ -30,7 +30,7 @@ void MCNPGeometry::associateCell2Density()
   std::string lineLower;
   std::transform(std::begin(currentLine), std::end(currentLine),
                  std::back_inserter(lineLower),
-                 [](char c) { return std::tolower(c); });
+                 [](unsigned char c) { return std::tolower(c); });
   std::string::size_type const pos = lineLower.find("like");
 
   std::istringstream iss(currentLine);
@@ -52,6 +52,10 @@ void MCNPGeometry::associateCell2Density()
       iss >> density;
       // remove trailing brackets
       density = density.substr(0, density.find('('));
+      // convert to lowercase
+      std::transform(std::begin(density), std::end(density),
+                     std::begin(density),
+                     [](unsigned char c){ return std::tolower(c); });
       std::istringstream istest(density);
       double fdensity;
       istest >> fdensity;
@@ -89,7 +93,7 @@ void MCNPGeometry::handleLikeNBut(unsigned long cellNum, std::string::size_type 
     std::string tokenLower;
     std::transform(std::begin(token), std::end(token),
                    std::back_inserter(tokenLower),
-                   [](char c) { return std::tolower(c); });
+                   [](unsigned char c) { return std::tolower(c); });
     if(tokenLower.substr(0, 4) == "rho=") {
       newValue.second = token.substr(4);
     } else if(tokenLower.substr(0, 4) == "mat=") {

t4_geom_convert/Kernel/FileHandlers/Parser/ParseMCNPCell.py
@@ -31,6 +31,7 @@ from ...Progress import Progress
 from ...Volume.CellMCNP import CellMCNP
 from ...Volume.Lattice import parse_ranges, LatticeSpec
 from ...Transformation.Transformation import get_mcnp_transforms
+from ...Utils import normalize_float
 
 
 class ParseMCNPCellError(Exception):
@@ -173,7 +174,7 @@ class ParseMCNPCell:
         if kws['material'] is not None:
             material_id = kws['material']
         if kws['density'] is not None:
-            density = kws['density']
+            density = normalize_float(kws['density'])
         fillid = self.to_fillid(kws, lat_opt)
         kws['trcl'] = [] if not kws['trcl'] else [kws['trcl']]
 
@@ -188,7 +189,7 @@ class ParseMCNPCell:
         if int(material_id) == 0:
             density = None
         else:
-            density = material.split()[1]
+            density = normalize_float(material.split()[1])
         return material_id, density
 
     @staticmethod